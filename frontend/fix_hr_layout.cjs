48ce6944f0ca3cab323e7778b36f056d0ee79eb2
premnankani99 <premnankani99@gmail.com>
2026-07-22 18:52:24 +0530

    Cleaned up all temporary testing scripts

--- frontend/fix_hr_layout.cjs
+++
@@ -1,9 +0,0 @@
-const fs = require('fs');
-
-// 1. Fix Layout.jsx HR Leave History link
-let layoutContent = fs.readFileSync('src/components/layout/Layout.jsx', 'utf8');
-layoutContent = layoutContent.replace(/<Link to="\/hr\/leaves"/g, '<Link to="/hr/history"');
-layoutContent = layoutContent.replace(/isActive\('\/hr\/leaves'\)/g, "isActive('/hr/history')");
-fs.writeFileSync('src/components/layout/Layout.jsx', layoutContent, 'utf8');
-console.log('Fixed Layout.jsx HR link');
-
